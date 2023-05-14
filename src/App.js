0dd11ad1707859235ecf68d4951c3b025840eb44
Eliel <46634774+elielnfinic@users.noreply.github.com>
2023-05-14 15:36:55 +0200

    Some progress here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import * as bip39 from "@scure/bip39";
+import * as bip32 from "@scure/bip32";
+import {grindKey, getPublicKey} from "micro-starknet";
 
 function App() {
+  const get_mnemonic = async () => {
+    const mnemonic = process.env.REACT_APP_MNEMO_PHRASE;
+    const seed = await bip39.mnemonicToSeed(mnemonic);
+    let hdnode = bip32.HDKey.fromMasterSeed(seed);
+
+    const accountIndex = 0;
+
+    hdnode = hdnode.derive(`m/44'/9004'/0'/0'/${accountIndex}`);
+    console.log(hdnode);
+
+    const res = grindKey(hdnode.privateKey);
+    const pub_key = getPublicKey(res);
+    console.log(res);
+    console.log("Public key is ", pub_key.toString());
+
+    return hdnode;
+
+    //const pkey = doEIP2645Hashing(hdnode.privateKey);
+  }
+
+  get_mnemonic();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Yes 
     </div>
   );
 }
